90885ba089e197b97501237820259b79f97d032f
EstoesMoises <contactomoises1@gmail.com>
2023-10-30 23:17:04 +0000

    ChampionList component basics done

--- src/components/ChampionsList.tsx
+++ src/components/ChampionsList.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { FetchChampionsData } from "../utils/champions";
 import ChampionCard from "./ChampionCard";
-import { Suspense } from "react";
 
 type Champion = {
   name: string;
@@ -12,20 +11,26 @@ type Champion = {
 function ChampionsList() {
   const [champions, setChampions] = useState<Champion[]>([]);
   const [loading, isLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [CDNerror, setCDNerror] = useState(false);
 
   useEffect(() => {
     setChampionsData();
-
     async function setChampionsData() {
-      const data = await FetchChampionsData();
-      setChampions(Object.values(data.data));
+      try {
+        const data = await FetchChampionsData();
+        setChampions(Object.values(data.data));
+        isLoading(false);
+      } catch (error) {
+        setCDNerror(true);
+        isLoading(false);
+        console.error(`Error fetching riot's API data, the error is: ${error}`);
+      }
     }
   }, []);
 
   if (loading) {
     return <p>Loading...</p>;
-  } else if (error) {
+  } else if (CDNerror) {
     return (
       <p>
         There was an error fetching the data from Riot's API, try again or
